a6fac3ed100c12f7f076e7a6c9e5ec0093b404e2
Ben <ben.k.richter@gmail.com>
2015-07-20 23:41:20 +0200

    add blacklist button, check every 2 seconds

--- js/inject.js
+++ js/inject.js
@@ -3,23 +3,83 @@ console.log("Skip Tracks - 8tracks is here...");
 
 jQuery(function($) {
 
+    // Do on song loading    (stops after jquery fail ??)
+//    $(document).ajaxComplete(function(event,xhr, settings){
+//        console.log("ajaxComplete");
+//        console.log(event);
+//        console.log(xhr);
+//        console.log(settings);
+//
+//        addButton();
+//    });
 
 
-    // TODO: checks if song is playing
-    var playing = false;
+    var $favLink;
+    var trackID = 0;
+    // check every 2 second for new song
     window.setInterval(function(){
-        if($("#now_playing").length){
-            playing = true;
-            console.log("playing");
-        }else{
-            console.log("nope...");
+        $favLink = $("#player #now_playing a.fav");
 
+        if(trackID !== $favLink.data("track_id")){
+            trackID = $favLink.data("track_id");
+            addButton();
+            console.log("new track");
         }
-    }, 1000);
+
+        console.log(trackID);
+
+    }, 2000);
 
 
-        console.log("after while");
-        $('#now_playing li.track').prepend('<a class="skip" href="/skip-dat"><span class="i-favorite"></span></a>');
+    /**
+     *  add the blacklist button
+     */
+    function addButton (){
+        $favLink.after('<a href="/toggle_black" class="black inactive" data-method="post" data-track_id="' + trackID + '" title="Add this track to your blacklist">' +
+                            '<span class="i-x"></span>' +
+                        '</a>');
+
+       // trackID
+
+    }
+
+    /**
+     * check current song if on List
+     */
+    function checkOnList (){
+
+
+        // set button active
+    }
+
+
+
+
+    /**
+     * put song on blacklist
+     */
+    function blacklistSong(){
+
+        // listen to button skip
+
+        // set active
+
+    }
+
+    /**
+     * trigger the skip event/link
+     */
+    function skipSong(){
+
+
+    }
+
+
+       // $('#now_playing li.track').prepend('<a class="skip" href="/skip-dat"><span class="i-favorite"></span></a>');
 
 
 });
+
+
+
+// GET https://api.soundcloud.com/tracks/6074695/stream?client_id=3904229f42df3999df223f6ebf39a8fe
